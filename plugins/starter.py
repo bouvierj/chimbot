a2ca65ac9d2fabaf7ea09c8e7127c01f842d22b1
bouvierj <bouvierj@users.noreply.github.com>
2016-03-17 11:40:27 -0400

    Update starter.py

--- plugins/starter.py
+++ plugins/starter.py
@@ -85,7 +85,7 @@ def process_message(data):
     	rollinfo = re.sub("C(?i)himbot[\s]*roll[\s]*me[\s]*", '', rollinfo)
     	outputs.append([data['channel'], "You input: " + rollinfo])
     	if re.match("\dd(?i)(\d|\d\d|\d\d\d|\%|f(?i)duge)"):
-    		outputs.append(data['channel'], "Valid"
+    		outputs.append(data['channel'], "Valid")
     	else:
     		outputs.append(data['channel'], "Invalid"
     	
