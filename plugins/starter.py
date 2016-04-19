464e4405693b1ad497a09d67ae131c28ce4fc31c
bouvierj <bouvierj@users.noreply.github.com>
2016-04-19 12:41:56 -0400

    Update starter.py

--- plugins/starter.py
+++ plugins/starter.py
@@ -141,7 +141,9 @@ def process_message(data):
     		outputs.append([data['channel'], e])
     		
     elif data['user'].startswith("U0KH7TRCG") and data['channel'].startswith("D") and data['text'].lower().startswith("chimbot say"):
-    	outputs.append(['G0KQ3BQ30', "Test"])
+    	messageinfo = str(data['text'])
+    	messageinfo = re.sub("C(?i)himbot[\s]*say[\s]*", '', messageinfo)
+    	outputs.append(['G0KQ3BQ30', messageinfo])
     	
     elif data['user'].startswith("U0KH7TRCG") and data['text'].lower().find("cool") != -1:
     	outputs.append([data['channel'], "Cool Cool Cool."])
